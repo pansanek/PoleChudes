ffaac06a8e7f1c517503cc148c76820547915f58
pansanek <alex.potjomkin@gmail.com>
2022-10-08 13:50:01 +0300

    Добавление функций создания объекта пользователя
    Тесты проходят успешно

--- src/Player.java
+++ src/Player.java
@@ -13,23 +13,23 @@ public class Player {
 
 
     public String getName() {
-
+        return name;
     }
 
     public void setName(String name) {
-
+        this.name = name;
     }
 
 
     public int getScore() {
-
+        return score;
     }
 
     public void setScore(int score) {
-
+        this.score = score;
     }
 
     public void increaseScore(int points) {
-
+        this.score += points;
     }
 }
