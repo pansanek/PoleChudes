2b5352e0dd1d50ee400918bcef0f08180ae2bc78
Onestus <vkryazhevskikh@gmail.com>
2022-10-08 14:25:10 +0300

    Создал тесты
    Класс проверяет корректность работы функций getTask и getAnswer

--- src/DataTest.java
+++ src/DataTest.java
@@ -1,5 +1,26 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class DataTest {
 
+    @Test
+    void getTask() {
+        boolean difficult = false;
+        Data data;
+        data = new Data(difficult);
+        String Answer = data.getTask(0);
+        assertEquals("Под каким сценическим именем более известна популярная российская певица Дарья Сергеевна Шиханова",Answer);
+        assertNotEquals("Фамилия ректора МИРЭА — Российский технологический университет",Answer);
+    }
+
+    @Test
+    void getAnswer() {
+        boolean difficult = false;
+        Data data;
+        data = new Data(difficult);
+        String Answer = data.getAnswer(0);
+        assertEquals("дора",Answer);
+        assertNotEquals("кудж",Answer);
+    }
 }
