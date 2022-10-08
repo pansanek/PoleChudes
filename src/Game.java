7a0b9dd878dea8fe1f11573840c8cdc7ffdc170d
SashaPotemkin <alex.potjomkin@gmail.com>
2022-10-08 18:03:25 +0300

    Новые тесты
    Добавил тесты для инициализации прогресса.
    Дописал игру и тесты для оставшихся функций:
    1)Ввод  буквы
    2) Выбор варианта хода игрока

--- src/Game.java
+++ src/Game.java
@@ -2,16 +2,20 @@ import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
+import static java.lang.System.exit;
+
 public class Game {
     private int numberOfPlayers;
-
     private Drum drum;
     private Scanner scanner;
     private boolean difficult;
     private Player[] players;
     private String task;
     private String answer;
-    
+    private char[] progress;
+    private boolean endGame = false;
+    private int winner = 0;
+
     public void start() {
         Random random = new Random();
         Data data;
@@ -64,6 +68,29 @@ public class Game {
 
         //Создание строки, в которой будет отображаться прогресс отгадывания слова
         initializationOfProgress(answer.length());
+
+        //Игроки играют пока не настанет конец игры
+        while (endGame == false) {
+
+            for (int i = 0; i < players.length; i++) {
+                System.out.println("Ваш ход " + players[i].getName());
+                System.out.println("Крутите барабан!");
+
+                //Определение выпавшего числа на барабане
+                readDrum(drum.roll(), i);
+                System.out.println(players[i].getScore() + " очков");
+
+                //Напоминание задания и текущего погресса в слове
+                printTaskAndProgress();
+
+                //Ход игрока
+                playerStep(i);
+            }
+        }
+
+        //Конец игры
+        System.out.println("Поздравляю " + players[winner].getName() + ", вы победили! Общее количество: " + players[winner].getScore());
+        exit(1);
     }
 
     private void initializationOfPlayers(int number) {
@@ -83,5 +110,14 @@ public class Game {
             players[i].setScore(0);
 
         }
+    }
+
+    private void initializationOfProgress(int size) {
+        //Изначально прогресс отгадывания слова 0 и отображаются только *
+        progress = new char[size];
+        for (int i = 0; i < progress.length; i++) {
+            progress[i] = '*';
+        }
+
     }
 }
--- src/GameTest.java
+++ src/GameTest.java
@@ -1,14 +1,36 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import static java.lang.Character.toUpperCase;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
     private char[] progress;
+    private Scanner scanner;
+    private String answer;
+    private Player[] players;
     @Test
     void TestInitializationOfProgress() {
         String Answer = initializationOfProgress(8);
         assertEquals("********", Answer);
     }
+    @Test
+    void TestPlayerStep() {
+        String Answer = playerStep(3);
+        assertEquals("Нет такого варианта. Попробуйте еще раз", Answer);
+    }
+
+    @Test
+    void TestEnterLetter() {
+        String Answer = enterLetter('а');
+        assertEquals("а*******", Answer);
+    }
+
+    /*------------------------------------------------------------------------------------
+       Далее идут методы созданные только для тестов, которые будут исправлены в обычном коде
+       ------------------------------------------------------------------------------------*/
 
     private String initializationOfProgress(int size) {
         //Изначально прогресс отгадывания слова 0 и отображаются только *
@@ -18,5 +40,48 @@ class GameTest {
         }
         String ProgressString = String.valueOf(progress);
         return ProgressString;
+    }
+
+
+    private String enterLetter(Character letter ) {
+        int totalEntries = 0;
+        answer = "апельсин";
+        progress = new char[8];
+        for (int i = 0; i < progress.length; i++) {
+            progress[i] = '*';
+        }
+        //Проверяем наличие введенной буква в нашем слове
+        for (int i = 0; i < answer.length(); i++) {
+            if (answer.charAt(i) == letter || answer.charAt(i) == toUpperCase(letter)) {
+                totalEntries++;
+                progress[i] = letter;
+            }
+        }
+        String returnn = String.valueOf(progress);
+        return returnn;
+    }
+
+
+    private String playerStep(int scan) {
+        scanner = new Scanner(System.in);
+        //System.out.println("Назовите слово или букву? Введите 1/2");
+        int answer = 0;
+
+        //Считываем вариант отгадывания слова
+        boolean correct = false;
+        while (correct == false) {
+            try {
+                answer = scan;
+
+                //Если ответ вне диапазона доступных
+                if (answer != 1 && answer != 2) {
+                    return ("Нет такого варианта. Попробуйте еще раз");
+                }
+                correct = true;
+            } catch (InputMismatchException e) {
+                return ("Неправильный параметр. Попробуйте еще раз");
+            }
+        }
+        return null;
     }
 }
